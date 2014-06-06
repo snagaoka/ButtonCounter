47b63180ed9da9fedb581572ef218ecf1d3cee4b
Shannon Nagaoka <shannonnagaoka@gmail.com>
2014-06-05 21:07:57 -1000

    cleaned up html, added SIAF to js

--- button_counter.js
+++ button_counter.js
@@ -1,10 +1,13 @@
-var counter = document.getElementById("counter");
-var button = document.getElementById("count_but");
-var count = 0;
+(function () {
 
-button.onclick = function increase() {
-	count++;
-	counter.innerHTML= count;
-	console.log(counter.innerHTML);
-}
+	var counter = document.getElementById("counter");
+	var button = document.getElementById("count_but");
+	var count = 0;
 
+	button.onclick = function increase() {
+		count++;
+		counter.innerHTML = count;
+		console.log(counter.innerHTML);
+	}
+
+})();
